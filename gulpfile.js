8fdf77bd59cd1188edb997ac29ee9adc793bd8c4
Anthony2511 <anthonybeaumecker@gmail.com>
2017-02-09 16:26:37 +0100

    Gulp task for scss

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,10 @@
 */
 
 var gulp = require( "gulp" ),
-    image = require( "gulp-image" );
+    image = require( "gulp-image" ),
+    sass = require( "gulp-sass" ),
+    autoprefixer = require( "gulp-autoprefixer" ),
+    csso = require( "gulp-csso" );
 
 // --- Task for images
 
@@ -15,7 +18,16 @@ gulp.task( "images", function() {
         .pipe( image() )
         .pipe( gulp.dest( "assets/images" ) );
 } );
+
 // --- Task for styles
+
+gulp.task( "css", function() {
+    gulp.src( "src/sass/**/*.scss" )
+        .pipe( sass().on( "error", sass.logError ) )
+        .pipe( autoprefixer() )
+        .pipe( csso() )
+        .pipe( gulp.dest( "assets/css" ) );
+})
 
 // --- Task for pug
 
