ed1d3f51c38a9e18a126535648033009f9e93bac
Anthony2511 <anthonybeaumecker@gmail.com>
2017-02-09 16:06:08 +0100

    Gulp task for images

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,16 @@
 * 09/02/2017
 */
 
-var gulp = require( "gulp" );
+var gulp = require( "gulp" ),
+    image = require( "gulp-image" );
 
 // --- Task for images
 
+gulp.task( "images", function() {
+    gulp.src( "src/images/**" )
+        .pipe( image() )
+        .pipe( gulp.dest( "assets/images" ) );
+} );
 // --- Task for styles
 
 // --- Task for pug
